683f08cf3c75fd3e785aff9ebc107ec4576cf4d0
CLSgGhost <61725416+CLSgGhost@users.noreply.github.com>
2020-03-26 00:27:57 +0800

    添加中缀表达式转为逆波兰式的函数

--- Exercises/Calculator.cpp
+++ Exercises/Calculator.cpp
@@ -2,7 +2,7 @@
 
 bool Calculator::isOperator(char ope)
 {
-	if (ope == '+' || ope == '-' || ope == '*' || ope == '/')
+	if (ope == '+' || ope == '-' || ope == '*' || ope == '/' || ope == '(' || ope == ')')
 		return true;
 	else
 		return false;
@@ -12,6 +12,8 @@ int Calculator::getPriority(char ope)
 {
 	if (isOperator(ope) == true)
 	{
+		if (ope == '(' || ope == ')')
+			return 0;
 		if (ope == '+' || ope == '-')
 			return 1;
 		if (ope == '*' || ope == '/')
@@ -37,13 +39,66 @@ double Calculator::calculate(double x1, char op, double x2)
 	}
 }
 
-string Calculator::toReversePolish(string expression)
+string Calculator::toReversePolish(string expression)  //传入中缀表达式转为逆波兰式
 {
-	return string();
+	std::stack<char> S1;  //运算符栈
+	std::stack<char> S2;  //逆波兰式栈
+	char ope;
+
+	for (int i = 0; i != expression.length(); i++)  //从expression中逐个取出字符
+	{
+		if (isOperator(expression[i]))  //若取出运算符
+		{
+			if (S1.empty() || expression[i] == '(')
+			{
+				S1.push(expression[i]);   //若栈空或运算符为'('直接入栈S1
+			}
+			else if(expression[i] == ')')
+			{
+				do
+				{   //若提取到')'，将栈S1中的运算符逐个取出
+					ope = S1.top();   S1.pop();
+					if (ope != '(')
+					{
+						S2.push(ope);  //若取出的不是'('，则压入栈S2
+					}
+				} while (ope != '(');  //取出'('后停止循环，并抛弃'('
+			}
+			else if (getPriority(expression[i]) > getPriority(S1.top()))  //比较优先级
+			{
+				S1.push(expression[i]);  //该运算符优先级比栈顶运算符优先级高，直接入栈
+			}
+			else
+			{
+				do
+				{   //从运算符栈S1逐个取出运算符并压入表达式栈S2，直到该运算符优先级比栈顶运算符优先级高
+					ope = S1.top();   S1.pop();
+					S2.push(ope);  
+				} while (getPriority(expression[i]) > getPriority(S1.top()));
+				S1.push(expression[i]);   //完成操作后该运算符优先级比栈顶运算符优先级高，入运算符栈S1
+			}
+		}
+		else
+		{
+			S2.push(expression[i]);   //若取出数字，直接入表达式栈S2
+		}
+	}
+	while (!S2.empty())   //将表达式栈逆序
+	{
+		ope = S2.top();   S2.pop();
+		S1.push(ope);
+	}
+	for (int j = 0; !S1.empty(); j++)  //输出逆波兰式
+	{
+		expression[j] = S1.top();   S1.pop();
+	}
+	return expression;
 }
 
-BinaryTreeNode * Calculator::toTree(string exp)
+BinaryTreeNode * Calculator::toTree(string exp)  //逆波兰式转为树
 {
+	BinaryTreeNode *exp_root;
+
 	return nullptr;
 }
 
@@ -58,7 +113,7 @@ double Calculator::calcResult(BinaryTreeNode * root)
 		calcResult(root->rightChild);
 		if (isOperator(root->data.at(0)))
 		{
-			num.push(root->data.at(0));    //运算符压入num栈
+			num.push(root->data.at(0));    //数字压入num栈
 		}
 		else
 		{   //结点为运算符时进行运算
